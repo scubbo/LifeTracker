7b4760c41d5d7f474316fff2424304173a5a2b63
Jack Jackson <jackjack@amazon.com>
2015-02-16 13:54:38 +0000

    Simplify if statement

--- app/src/main/java/com/scubbo/lifetracker/app/MainActivity.java
+++ app/src/main/java/com/scubbo/lifetracker/app/MainActivity.java
@@ -49,13 +49,8 @@ public class MainActivity extends ActionBarActivity implements View.OnTouchListe
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     @Override
